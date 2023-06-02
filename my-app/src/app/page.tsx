f4e0e95e08515533369d31fa0871a86873812efb
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-02 03:51:58 +0330

    wrap main page in layout

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,11 @@
-
 export default function Home() {
   return (
-   <h1>home</h1>
-  )
+    <div className="flex flex-col h-full">
+      <header className="h-14 p-10 bg-green-300 text-center">فروشگاه</header>
+      <main className="flex-1 h-full p-10">
+        <h1>صفحه اصلی</h1>
+      </main>
+      <footer className="bg-green-500 h-20 text-center">فوتر</footer>
+    </div>
+  );
 }
