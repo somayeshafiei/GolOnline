4f17a5d9b7af311e9dfa77dc1f646c73f6046161
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-06 01:14:24 +0330

    add responsive header navbar and drawer

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,21 +1,19 @@
 'use client';
 import React, { useState } from 'react';
-import { Drawer, Menu } from 'antd';
-import { MenuOutlined } from '@ant-design/icons';
+import { Button, Drawer, Menu } from 'antd';
+import { MenuOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import Image from 'next/image';
+import logo from '../../images/Logo.png';
+import Link from 'next/link';
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
   return (
-    <header className="h-16 flex w-full border-b sm:justify-center">
-      <div
-        style={{
-          backgroundColor: 'red',
-          height: '60px',
-          paddingRight: 12,
-          paddingTop: 12,
-        }}
-        className="sm:hidden"
-      >
+    <header className="h-16 flex w-full border-b justify-between px-5 sm:px-10 md:px-[120px]">
+      <span className="hidden sm:flex sm:flex-col sm:justify-center sm:items-center  sm:justify-items-end">
+        <Image src={logo} alt="logo" height={60} width={120} />
+      </span>
+      <div className="order-first flex flex-col justify-center text-[#46A358] h-full sm:hidden">
         <MenuOutlined
           dir="rtl"
           style={{ fontSize: 30 }}
@@ -33,12 +31,21 @@ const Header = () => {
           setOpenMenu(false);
         }}
         closable={false}
-        bodyStyle={{ backgroundColor: 'blue' }}
+        bodyStyle={{ backgroundColor: 'blue', width: '190px' }}
+        contentWrapperStyle={{ width: '190px' }}
       >
         <AppMenue isInLine />
       </Drawer>
-      {/* <h1>tesssssst</h1> */}
-      {/* <img src=''/> */}
+      <div className="flex justify-center items-center gap-4">
+        <Link href={'/cart'}>
+          <ShoppingCartOutlined style={{ fontSize: 20 }} />
+        </Link>
+        <Link href={'/login'}>
+          <Button className="text-white bg-[#46A358] hover:bg-white hover:text-[#46A358] hover:border hover:border-[#46A358]">
+            ورود
+          </Button>
+        </Link>
+      </div>
     </header>
   );
 };
