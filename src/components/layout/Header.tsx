d55d347c4306c8a2668b5e96e6b0c5c0a103fa1c
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-05 17:58:15 +0330

    install ant-design/icons package and build menu of header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import { Drawer, Menu } from 'antd';
 
 const Header = () => {
-  return <header>Header</header>;
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <header className="h-16 ">
+      <div>
+        {/* <MenuOutlined /> */}
+      </div>
+      <AppMenue />
+      <Drawer
+        open={openMenu}
+        onClose={() => {
+          setOpenMenu(false);
+        }}
+        closable={false}
+        bodyStyle={{ backgroundColor: 'blue' }}
+      >
+        <AppMenue isInLine />
+      </Drawer>
+      {/* <h1>tesssssst</h1> */}
+      {/* <img src=''/> */}
+    </header>
+  );
 };
 
+function AppMenue({ isInLine = false }) {
+  return (
+    <Menu
+      style={{ height: '100%' }}
+      mode={isInLine ? 'inline' : 'horizontal'}
+      items={[
+        {
+          label: 'خانه',
+          key: 'Home',
+        },
+        {
+          label: 'محصولات',
+          key: 'Products',
+        },
+        {
+          label: 'وبلاگ',
+          key: 'Blog',
+        },
+        {
+          label: 'درباره ما',
+          key: 'About',
+        },
+      ]}
+    ></Menu>
+  );
+}
 export default Header;
