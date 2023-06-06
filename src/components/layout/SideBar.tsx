687c6df8b3a5a383fbbee4674801c785fafb67c0
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-06 23:47:16 +0330

    changr route by clicking menuItems of dashboard sidebar

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -8,15 +8,24 @@ import {
 } from '@ant-design/icons';
 import { Menu } from 'antd';
 import Title from 'antd/es/typography/Title';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const SideBar = () => {
+  const router = useRouter();
   return (
     <div className="h-full">
       <Title>داشبورد</Title>
       <Menu
+        onClick={({ key }) => {
+          if (key === '/Logout') {
+            router.push('/');
+          } else {
+            router.push(key);
+          }
+        }}
         items={[
-          { label: 'خانه', key: '/', icon: <HomeOutlined /> },
+          { label: 'خانه', key: '/dashboard', icon: <HomeOutlined /> },
           {
             label: 'سفارشات',
             key: '/dashboard/orders',
