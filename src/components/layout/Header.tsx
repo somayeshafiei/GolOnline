0568653ce6e7608288bd044c1111b9c864718a17
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-08 01:45:39 +0330

    build ProductsTable and add it to dasboard/products route

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <header className="h-16 flex w-full border-b justify-between px-5 sm:px-10 md:px-[120px]">
-      <span className="hidden sm:flex sm:flex-col sm:justify-center sm:items-center  sm:justify-items-end">
+      <span className="hidden sm:flex sm:flex-col sm:justify-center sm:items-center sm:justify-items-end">
         <Image src={logo} alt="logo" height={60} width={120} />
       </span>
       <div className="order-first flex flex-col justify-center text-[#46A358] h-full sm:hidden">
@@ -40,7 +40,7 @@ const Header = () => {
       >
         <AppMenue isInLine />
       </Drawer>
-      <div className="flex justify-center items-center gap-4">
+      <div className="flex items-center justify-center gap-4">
         <Link href={'/cart'}>
           <ShoppingCartOutlined style={{ fontSize: 20 }} />
         </Link>
--- src/components/pages/dashboard/Table.tsx
+++ src/components/pages/dashboard/Table.tsx
@@ -1,7 +1,101 @@
-import React from 'react';
+// import React from 'react';
 
-const Table = () => {
-  return <div>Table</div>;
-};
+// const Table = () => {
+//   return <div>Table</div>;
+// };
 
-export default Table;
+// export default Table;
+'use client';
+interface Props {
+  products: Prduct[];
+}
+interface Prduct {
+  id: string;
+  category: string;
+  subcategory: string;
+  name: string;
+  price: number;
+  brand: string;
+  description: string;
+  thumbnail: string;
+  images: string[];
+  rating: {
+    rate: number;
+    count: number;
+  };
+
+  createdAt: string;
+
+  updatedAt: string;
+
+  slugname: string;
+
+  __v: number;
+}
+import instance from '@/api/constants';
+import { Button, Table } from 'antd';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+export default function Test({ products }: Props) {
+  // const [categories, setCategories] = useState();
+  const [columns, setColumns] = useState([
+    {
+      title: 'تصویر',
+      dataIndex: 'images[0]',
+      // render:()=><img src={`https://localhost:8000/products/images/products/images/${dataIndex}`}
+    },
+    {
+      title: 'نام محصول',
+      dataIndex: 'name',
+    },
+    {
+      title: 'دسته بندی',
+      dataIndex: 'category',
+      // render:(value:string)=>fetch()
+    },
+    {
+      title: 'عملیات',
+      dataIndex: 'actions',
+      render: () => (
+        <div className="flex items-center justify-center w-full h-full gap-3">
+          <Button type="primary">ویرایش</Button>
+          <Button>حذف</Button>
+        </div>
+      ),
+    },
+  ]);
+  const [dataSource, setDataSource] = useState(
+    products && Array.isArray(products) ? products : []
+  );
+  // useEffect(() => {
+  //   axios
+  //     .get('http://localhost:8000/api/categories')
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response.data);
+  //       if (response.data.status === 'success') {
+  //         setCategories(response?.data?.categories);
+  //       }
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     });
+  // });
+  // useEffect(() => {
+  //   console.log(categories);
+  //   products.forEach((pro) => {
+  //      categories?.find((ctg) => ctg.id === pro.id);
+  //   });
+  // }, [products, categories]);
+  return (
+    <>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        bordered
+        scroll={{ y: 500 }}
+      />
+    </>
+  );
+}
