590620ec886f7e8cbf1359a5f6b4eab88458df1d
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-09 13:58:12 +0330

    change url of fetching in dashboard/Inventory route

--- src/app/(admin)/dashboard/Inventory_price/page.tsx
+++ src/app/(admin)/dashboard/Inventory_price/page.tsx
@@ -1,6 +1,6 @@
 import InventoryTable from '@/components/pages/dashboard/InventoryTable';
 export async function getData() {
-  const res = await fetch('http://localhost:8000/api/products?limit=all');
+  const res = await fetch('http://localhost:8000/api/products');
   // page=1&limit=4&fields=-rating,-createdAt,-updatedAt,-__v&sort=price&quantity[gte]=8
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
