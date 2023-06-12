8ef320b7b1a87adc97c561e05e48b06208f2cbae
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-12 20:25:55 +0330

    add interceptor for axios

--- src/api/constants/index.tsx
+++ src/api/constants/index.tsx
@@ -3,5 +3,53 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://localhost:8000/api/',
 });
+instance.interceptors.request.use((config) => {
+  // console.log(config);
+  if (config.url !== 'auth/token') {
+    const cookies = new Cookies(config.headers.cookie);
+    cookies.get('accessToken');
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    const config = error.config;
+    if (error.response.status === 401 && !config.sent) {
+      config.sent = true;
+      if (config.url !== 'auth/token') {
+        const cookies = new Cookies(config.headers.cookie);
+        const refreshToken = cookies.get('refreshToken');
+        instance
+          .post(
+            'auth/token',
+            {},
+            {
+              headers: {
+                Authorization: refreshToken,
+              },
+            }
+          )
+          .then((res) => {
+            const accessToken = res.data.accessToken;
+            const cookies = new Cookies();
+            cookies.set('accessToken', accessToken, { path: '/' });
+            cookies.set('refreshToken', res.data.refreshToken, { path: '/' });
+            config.headers.Authorization = accessToken;
+            return instance(config);
+          });
+      } else if (config.url === 'auth/token') {
+        const cookies = new Cookies();
+        cookies.remove('accessToken', { path: '/' });
+        cookies.remove('refreshToken', { path: '/' });
+      }
+    }
+  }
+);
 
 export default instance;
+
--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -7,7 +7,6 @@ export default function shopLayout({
 }) {
   return (
     <div className="flex flex-col h-full">
-      {/* <header className="h-14 p-10 bg-green-300 text-center">فروشگاه</header> */}
       <Header />
       <main className="flex-1 h-full">{children}</main>
       <footer className="bg-green-500 h-20 text-center">فوتر</footer>
