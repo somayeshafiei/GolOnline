383d831560caf6d95b4236b0b93138a8c36b2885
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-12 23:49:44 +0330

    change font of antd components

--- src/app/WrapperLayout.tsx
+++ src/app/WrapperLayout.tsx
@@ -1,11 +1,16 @@
 'use client';
 import { ConfigProvider } from 'antd';
-export default function WrapperLayout({ children }: { children: React.ReactNode }) {
+export default function WrapperLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ConfigProvider
       theme={{
         token: {
           colorPrimary: '#46a358',
+          fontFamily: 'iransans',
         },
       }}
     >
