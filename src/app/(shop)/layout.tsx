8924ac95eb72812511350998ed775ef7a415cb3a
Somaye Shafiei <S.shafiee76@gmail.com>
2023-06-23 14:44:10 +0330

    edit footer

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -9,7 +9,9 @@ export default function shopLayout({
     <div className="flex flex-col h-full">
       <Header />
       <main className="flex-1 h-full">{children}</main>
-      <footer className="bg-green-500 h-20 text-center">فوتر</footer>
+      <footer className="bg-green-500 h-20 text-center pt-4">
+        تمام حقوق برای green shop رزرو شده است
+      </footer>
     </div>
   );
 }
